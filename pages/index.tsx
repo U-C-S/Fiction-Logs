ab89d0a79b90b1ec03cf0c8e148856c72b0a5efe
Chanakya <ucs3721@outlook.com>
2022-05-28 19:54:35 +0000

    Add Tabs like ui and `tabler-icons` dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { createStyles } from "@mantine/core";
+import { createStyles, Tabs } from "@mantine/core";
 import React, { useState } from "react";
+import { Photo, Settings, Edit } from "tabler-icons-react";
 import { ProfileHeader } from "../components/core";
 
 const useStyles = createStyles(() => ({
@@ -7,20 +8,45 @@ const useStyles = createStyles(() => ({
 		padding: "1rem",
 		display: "flex",
 		justifyContent: "center",
+		alignItems: "center",
+		flexDirection: "column",
+
+		[".thetabs"]: {
+			marginTop: "1rem",
+			width: "clamp(320px, 75%, 600px)",
+		},
 	},
 }));
+
+function TheTabs() {
+	return (
+		<Tabs grow>
+			<Tabs.Tab label="Watched" icon={<Photo size={16} />}>
+				Gallery tab content
+			</Tabs.Tab>
+			<Tabs.Tab label="Planning" icon={<Edit size={16} />}>
+				Messages tab content
+			</Tabs.Tab>
+			<Tabs.Tab label="Settings" icon={<Settings size={16} />}>
+				Settings tab content
+			</Tabs.Tab>
+		</Tabs>
+	);
+}
 
 export default function IndexPage() {
 	const { classes } = useStyles();
 	const [profile, setProfile] = useState({
 		name: "Manté",
-		image: "https://avatars.dicebear.com/api/avataaars/chanakya555.svg"
+		image: "https://avatars.dicebear.com/api/avataaars/chanakya555.svg",
 	});
 
-
 	return (
-	<div className={classes.root}>
-		<ProfileHeader name={profile.name} image={profile.image} />
-	</div>
+		<div className={classes.root}>
+			<ProfileHeader name={profile.name} image={profile.image} />
+			<div className="thetabs">
+				<TheTabs />
+			</div>
+		</div>
 	);
 }
