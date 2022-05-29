a2ed5cebc8fb39a22df02bf14f03049e00f3a2b1
U-C-S <ucs3721@outlook.com>
2022-05-30 02:23:58 +0530

    Support Add UI for planning to watch

--- components/core/ControlsOverlay.tsx
+++ components/core/ControlsOverlay.tsx
@@ -1,15 +1,17 @@
-import { Affix, Button, createStyles, Modal, NumberInput, Paper, TextInput } from "@mantine/core";
+import { Affix, Button, Checkbox, Group, Modal, NumberInput, Paper, Stack, TextInput } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
+import { useToggle } from "@mantine/hooks";
 import { useState } from "react";
 import { Plus, Search } from "tabler-icons-react";
 
 export function ControlsOverlay() {
 	const [opened, setOpened] = useState(false);
+	const [type, toggle] = useToggle("Watched", ["Watched", "Planning"]);
 
 	return (
 		<Affix position={{ bottom: 20 }} style={{ width: "100%" }}>
 			<div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-				<Paper shadow="xs" radius="md" p="xs" withBorder>
+				<Paper shadow="xs" p="xs" withBorder>
 					<div style={{ display: "flex", justifyContent: "space-around", columnGap: "1rem" }}>
 						<Button leftIcon={<Plus />} onClick={() => setOpened(true)}>
 							Add
@@ -19,12 +21,29 @@ export function ControlsOverlay() {
 				</Paper>
 			</div>
 
-			<Modal opened={opened} onClose={() => setOpened(false)} title="Want to add something ?" centered>
-				<Paper radius="md" p="xl" withBorder>
+			<Modal opened={opened} onClose={() => setOpened(false)} title="Want to add something ?">
+				<Paper p="xl" withBorder>
 					<form>
-						<TextInput required label="Title" />
-						<NumberInput label="Rating" min={0} max={10} />
-						<DatePicker placeholder="Pick the date" label="Watched it on" inputFormat="YYYY-MM-DD" />
+						<Stack>
+							<TextInput required label="Title" />
+							<Checkbox
+								checked={type === "Planning"}
+								label="Planning to Watch ??"
+								onChange={() => toggle()}
+							/>
+							{type === "Watched" && (
+								<>
+									<Group grow>
+										<NumberInput label="Rating" placeholder="⭐ Give it a Rating" min={0} max={10} />
+										<DatePicker placeholder="Pick the date" label="Watched it on" inputFormat="YYYY-MM-DD" />
+									</Group>
+									<TextInput label="Comment" placeholder="What did you think about it ?" />
+								</>
+							)}
+						</Stack>
+						<Group position="right" mt="md">
+							<Button type="submit">Submit</Button>
+						</Group>
 					</form>
 				</Paper>
 			</Modal>
