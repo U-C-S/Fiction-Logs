457a403c69e91175f7ca65331a6cec8fd6eab83a
U-C-S <ucs3721@outlook.com>
2022-05-31 14:17:43 +0530

    loading screen css tweak

--- components/core/LoadingScreen.tsx
+++ components/core/LoadingScreen.tsx
@@ -3,7 +3,14 @@ import React from "react";
 
 export function LoadingScreen() {
 	return (
-		<Center style={{ width: "100vw", height: "100vh" }}>
+		<Center
+			style={{
+				width: "100vw",
+				height: "100vh",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+			}}>
 			<Loader color="red" variant="bars" />
 		</Center>
 	);
