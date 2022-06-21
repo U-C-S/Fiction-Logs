a09eea60b4edc9e2bc2ed90a893b361aebd2a645
u-c-s <uchanakyasrinivas@gmail.com>
2022-06-21 18:39:19 +0530

    Add pino-pretty, cors, jwt packages

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,24 +1,40 @@
-import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import fastify, { FastifyServerOptions } from "fastify";
 
 import prismaPlugin from "./plugins/prisma";
 import { filmListRoutes } from "./routes/filmlist";
 import { profileRoutes } from "./routes/profile";
 
-export const build = (opts = {}) => {
+export function buildFastifyServer(opts: FastifyServerOptions = {}) {
 	const app = fastify(opts);
 
 	app.register(prismaPlugin);
+	app.register(fastifyCors, {
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		origin: "*",
+	});
 
 	app.register(profileRoutes, { prefix: "/api/profile" });
 	app.register(filmListRoutes, { prefix: "/api/filmlist" });
 
 	return app;
+}
+
+let serverOpts: FastifyServerOptions = {
+	logger: {
+		transport: {
+			target: "pino-pretty",
+			options: {
+				ignore: "pid, hostname",
+				translateTime: "HH:MM:ss",
+			},
+		},
+	},
 };
 
 // server
-build({ logger: true }).listen({ port: parseInt(process.env.PORT || "3100") }, (err, address) => {
+buildFastifyServer(serverOpts).listen({ port: parseInt(process.env.PORT || "3100") }, (err, address) => {
 	if (err) {
 		console.log(err);
-		process.exit(1);
 	}
 });
