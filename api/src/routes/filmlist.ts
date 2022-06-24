9801f4f1374917756110123c3dbcbb0db509dcd5
u-c-s <uchanakyasrinivas@gmail.com>
2022-06-24 23:56:57 +0530

    Test add types for a api method and add update film api

--- api/src/routes/filmlist.ts
+++ api/src/routes/filmlist.ts
@@ -1,11 +1,25 @@
+import { film } from "@prisma/client";
 import { FastifyInstance } from "fastify";
 
 export async function filmListRoutes(fastify: FastifyInstance) {
 	let { prisma } = fastify;
 
-	fastify.get("/:name", async (request, reply) => {
-		const { name } = request.params as any;
-		const { watched } = request.query as any;
+	// Get all list of films of a user
+	fastify.get<{
+		Reply: {
+			success: boolean;
+			data: film[] | any;
+			message: string;
+		};
+		Params: {
+			name: string;
+		};
+		Querystring: {
+			watched?: string;
+		};
+	}>("/:name", async (request, reply) => {
+		const { name } = request.params;
+		const { watched } = request.query;
 
 		const films = await prisma.film.findMany({
 			where: {
@@ -20,9 +34,10 @@ export async function filmListRoutes(fastify: FastifyInstance) {
 			select: {
 				name: true,
 				created_at: true,
+				is_watched: true,
+				watched_on: watched == "true" ? true : false,
 				comment: watched == "true" ? true : false,
 				rating: watched == "true" ? true : false,
-				watched_on: watched == "true" ? true : false,
 			},
 		});
 
@@ -71,6 +86,30 @@ export async function filmListRoutes(fastify: FastifyInstance) {
 		return reply.send({
 			success: true,
 			message: `${filmname} has deleted from ${name}`,
+		});
+	});
+
+	// update film
+	fastify.put("/:name", async (request, reply) => {
+		const { name } = request.params as { name: string };
+		const { filmname } = request.body as any;
+
+		await prisma.film.update({
+			where: { name: filmname },
+			data: {
+				is_watched: true,
+				watched_on: new Date(),
+				comment: "",
+				rating: 0,
+				profile: {
+					connect: { name: name },
+				},
+			},
+		});
+
+		return reply.send({
+			success: true,
+			message: `${filmname} has updated to ${name}`,
 		});
 	});
 }
