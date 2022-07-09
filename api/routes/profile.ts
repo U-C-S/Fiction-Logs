11b9f8009a2436a25c39bd764a061a5d8182473e
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-10 02:15:58 +0530

    refactor `routes/profile.ts` to use services

--- api/routes/profile.ts
+++ api/routes/profile.ts
@@ -1,26 +1,25 @@
 import { FastifyInstance } from "fastify";
+import { getProfile } from "../services/profileServices";
 import { jwtUserPayload } from "../types/jwt";
 
 export async function profileRoutes(fastify: FastifyInstance) {
-	let { prisma } = fastify;
-
 	fastify.get(
 		"/",
 		{
 			onRequest: [fastify.authenticate],
 		},
 		async (request, reply) => {
-			let id = (request.user as jwtUserPayload).id;
-			const profile = await prisma.profile.findUnique({ where: { id } });
+			let name = (request.user as jwtUserPayload).username;
+			const result = await getProfile(name);
 
-			if (!profile) {
+			if (!result.success) {
 				return reply.code(404).send({ message: "Profile not found" });
 			}
 
-			let { name, created_at, email } = profile;
+			let { created_at, email } = result.data;
 			reply.code(200).send({
 				succes: true,
-				data: { id, name, created_at, email },
+				data: { name, created_at, email },
 			});
 		}
 	);
@@ -28,38 +27,15 @@ export async function profileRoutes(fastify: FastifyInstance) {
 	fastify.get("/:name", async (request, reply) => {
 		const { name } = request.params as { name: string };
 
-		const profile = await prisma.profile.findUnique({
-			where: { name },
-		});
-		if (!profile) {
+		const profile = await getProfile(name);
+
+		if (!profile.success) {
 			return reply.code(404).send({
 				success: false,
 				message: "Profile not found",
 			});
 		}
 
-		return reply.send({
-			success: true,
-			data: profile,
-		});
+		return reply.send(profile);
 	});
-	/*
-	fastify.post("/", async (request, reply) => {
-		const { name, email, password } = request.body as any;
-		let profile;
-		try {
-			profile = await prisma.profile.create({
-				data: {
-					name,
-					email,
-					password,
-				},
-			});
-		} catch (error) {
-			console.log(error);
-			let x = await prisma.profile.count();
-		}
-		return profile;
-	});
-	*/
 }
