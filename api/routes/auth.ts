771c07f263757b6b6c5ec64580e5aaa740c880f0
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-10 02:15:28 +0530

    refactor `routes/auth.ts` to use services

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,55 +1,58 @@
 import { FastifyInstance } from "fastify";
 import bcrypt from "@node-rs/bcrypt";
 
+import { createProfile, getProfile } from "../services/profileServices";
+
 export async function authRoutes(fastify: FastifyInstance) {
 	let { prisma } = fastify;
 
 	fastify.post("/login", async (request, reply) => {
 		const { name, password } = request.body as any;
 
-		const profile = await prisma.profile.findUnique({
-			where: { name },
-		});
+		const result = await getProfile(name);
 
-		let code: number;
-		if (!profile) code = 404;
-		else if (!(await bcrypt.compare(password, profile.password))) code = 403;
-		else code = 200;
-
-		if (code != 200) {
-			return reply.code(code).send({
-				success: false,
-				message: "Invalid credentials",
+		if (result.success && (await bcrypt.compare(password, result.data.password))) {
+			return reply.send({
+				success: true,
+				message: "Success",
+				data: {
+					token: await reply.jwtSign({
+						id: result.data.id as number,
+						username: name,
+					}),
+					username: name,
+				},
 			});
 		}
 
-		return reply.send({
-			success: true,
-			message: "Success",
-			data: {
-				token: await reply.jwtSign({
-					id: profile?.id as number,
-					username: name,
-				}),
-				username: name,
-			},
+		return reply.code(403).send({
+			success: false,
+			message: "Invalid credentials",
 		});
 	});
 
 	fastify.post("/register", async (request, reply) => {
 		const { name, password, email } = request.body as any;
 
-		await prisma.profile.create({
-			data: {
-				name,
-				password: await bcrypt.hash(password, 10),
-				email,
-			},
-		});
+		let hashedpassword = await bcrypt.hash(password, 10);
+		let profile = await createProfile({ name, password: hashedpassword, email });
 
-		return reply.send({
-			success: true,
-			message: "Success",
+		if (profile.success) {
+			return reply.send({
+				success: true,
+				message: "Success",
+				data: {
+					token: await reply.jwtSign({
+						id: profile.data?.id as number,
+						username: name,
+					}),
+					username: name,
+				},
+			});
+		}
+		return reply.code(400).send({
+			success: false,
+			message: "Invalid credentials",
 		});
 	});
 
