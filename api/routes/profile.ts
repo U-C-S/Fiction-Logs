2b91f97400a143547c637069414a1c875d5039fd
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-11 07:04:41 +0530

    Add back route `profile/me/all` (I need it)

--- api/routes/profile.ts
+++ api/routes/profile.ts
@@ -16,6 +16,20 @@ export async function profileRoutes(fastify: FastifyInstance) {
 		}
 	);
 
+	fastify.get(
+		"/me/all",
+		{
+			onRequest: [fastify.authenticate],
+		},
+		async (request, reply) => {
+			let name = (request.user as jwtUserPayload).username;
+
+			const result = await getProfile(name, true);
+
+			return reply.code(result.success ? 200 : 400).send(result);
+		}
+	);
+
 	fastify.get("/byname/:name", async (request, reply) => {
 		const { name } = request.params as { name: string };
 
