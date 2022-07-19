ce22d989889cb3a41c2d8d36d7eafb4ea6c2e587
u-c-s <uchanakyasrinivas@gmail.com>
2022-07-20 00:38:53 +0530

    Working `vercel severless` 🎉

--- api/api/serverless.js
+++ api/api/serverless.js
@@ -1,6 +1,6 @@
 import Fastify from "fastify";
 import * as dotenv from "dotenv";
-import init from "./app";
+import init from "../dist/app";
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ app.get("/", async (request, reply) => {
 	reply.send({ message: "Hello from ficition-logs api" });
 });
 
-export default async (req: any, res: any) => {
+export default async (req, res) => {
 	await app.ready();
 	app.server.emit("request", req, res);
 };
